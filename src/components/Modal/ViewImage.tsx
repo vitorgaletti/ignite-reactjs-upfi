4110d6bfe929a4f13da373f84c8751aa89d289be
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-23 18:13:20 -0300

    :white_check_mark: should be able to render images list

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,34 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
+      <ModalOverlay />
+      <ModalContent
+        mx="auto"
+        w="auto"
+        h="auto"
+        maxWidth={['300px', '500px', '900px']}
+        maxHeight={['350px', '450px', '600px']}
+      >
+        <ModalBody p="0">
+          <Image
+            src={imgUrl}
+            maxWidth={['300px', '500px', '900px']}
+            maxHeight={['350px', '450px', '600px']}
+          />
+        </ModalBody>
+        <ModalFooter
+          bg="pGray.800"
+          height="2rem"
+          py="20px"
+          borderBottomRadius="5px"
+        >
+          <Link href={imgUrl} isExternal fontSize="1rem" mr="auto">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,13 +50,9 @@ export default function Home(): JSX.Element {
     return formatted;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
-
   if (isLoading && !isError) {
     return <Loading />;
   }
-
-  // TODO RENDER ERROR SCREEN
 
   if (isLoading && isError) {
     return <Error />;
